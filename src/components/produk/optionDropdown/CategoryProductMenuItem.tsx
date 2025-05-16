b9a4f73cbb6d0970a81f10612e2c1c2dc6fb2f80
Hafid <hafid@gmail.com>
2025-05-16 20:24:13 +0700

    feat : refactor option and create page upload file exel

---
+++ src/components/produk/optionDropdown/CategoryProductMenuItem.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router";
+
+export default function CategoryProductMenuItem() {
+  return (
+    <Link
+      to="/produk/kategori_produk"
+      className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
+    >
+      <svg
+        className="fill-gray-500 group-hover:fill-gray-700 dark:group-hover:fill-gray-300"
+        width="30"
+        height="30"
+        fill="none"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          <path d="m74 42a2 2 0 0 1 2 1.85v28.15a6 6 0 0 1 -5.78 6h-40.22a6 6 0 0 1 -6-5.78v-28.22a2 2 0 0 1 1.85-2zm-15.5 8.34-.12.1-11.45 12.41-5.2-5a1.51 1.51 0 0 0 -2-.1l-.11.1-2.14 1.92a1.2 1.2 0 0 0 -.1 1.81l.1.11 7.33 6.94a3.07 3.07 0 0 0 2.14.89 2.81 2.81 0 0 0 2.13-.89l5.92-6.29.43-.44.42-.45.55-.58.21-.22.42-.44 5.62-5.93a1.54 1.54 0 0 0 .08-1.82l-.08-.1-2.14-1.92a1.51 1.51 0 0 0 -2.01-.1zm15.5-28.34a6 6 0 0 1 6 6v6a2 2 0 0 1 -2 2h-56a2 2 0 0 1 -2-2v-6a6 6 0 0 1 6-6z"></path>
+        </g>
+      </svg>
+      Kategori Produk
+    </Link>
+  );
+}
