9fef62bc64a0ec0b8dcab05cdef65ad9286d520f
vnonymous_ <61929120+vierynugroho@users.noreply.github.com>
2025-06-19 11:04:40 +0700

    Update Allexpense.tsx

--- src/pages/Expenses/Allexpense.tsx
+++ src/pages/Expenses/Allexpense.tsx
@@ -199,6 +199,12 @@ export default function AllExpense() {
                 type="text"
                 value={keyword}
                 onChange={(e) => setKeyword(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    e.preventDefault();
+                    handleSearch();
+                  }
+                }}
                 placeholder="Masukkan Nama pengeluaran"
                 className="dark:bg-dark-900 h-11 w-full md:w-10/12 rounded-lg border border-gray-200 bg-transparent py-2.5 pl-10 pr-14 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800  dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 md:pl-12"
               />
